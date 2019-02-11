5cc0bc3e29b1c7b05212ba67551189a4c94a1b01
Burizer <31881710+Burizer@users.noreply.github.com>
2019-02-11 10:58:50 +0100

    Fikset, delvist.

--- EntertainerManagement/Script.PostDeployment1.sql
+++ EntertainerManagement/Script.PostDeployment1.sql
@@ -101,34 +101,6 @@ WHEN NOT MATCHED BY TARGET THEN
 INSERT (ContactInfo_phoneNo,ContactInfo_phoneNoExt,ContactInfo_email,ContactInfo_type)
 VALUES (ContactInfo_phoneNo,ContactInfo_phoneNoExt,ContactInfo_email,ContactInfo_type);
 
-MERGE INTO AgentContactInformation AS TARGET
-USING (VALUES
-		(1,'48332123','+45','','Public'),
-		(2,'','','joemarkusson@gmail.com','Private'),
-		(3,'3491223174','+44','boblarsen@edumail.dk','Public'),
-		(4,'439213','+52','','Private'),
-		(5,'88372931','+45','sarahjoergensen@gmail.dk','Private')	
-)
-AS SOURCE (FK_AgentID,ContactInfo_phoneNo,ContactInfo_phoneNoExt,ContactInfo_email,ContactInfo_type)
-ON Target.FK_AgentID = Source.FK_AgentID
-WHEN NOT MATCHED BY TARGET THEN
-INSERT (FK_AgentID,ContactInfo_phoneNo,ContactInfo_phoneNoExt,ContactInfo_email,ContactInfo_type)
-VALUES (FK_AgentID,ContactInfo_phoneNo,ContactInfo_phoneNoExt,ContactInfo_email,ContactInfo_type);
-
-MERGE INTO Agent AS TARGET
-USING (VALUES
-		(1,'Joseph','Steiner',2000,'20140903',2),
-		(2,'Jan','Karlson',250.53,'20071014',4),
-		(3,'Marianne','Smith',400.74,'20180808',2),
-		(4,'Sabrina','Kriegsdottir',223.9,'9991229',3),
-		(5,'Kris','Lawrence',510.2,'20160102',1)	
-)
-AS SOURCE (Agent_ID, Agent_fName, Agent_lName, Agent_salary, Agent_hiringDate, FK_agencyOfficeID)
-ON Target.Agent_ID = Source.Agent_ID
-WHEN NOT MATCHED BY TARGET THEN
-INSERT (Agent_fName,Agent_lName,Agent_salary,Agent_hiringDate,FK_agencyOfficeID)
-VALUES (Agent_fName,Agent_lName,Agent_salary,Agent_hiringDate,FK_agencyOfficeID);
-
 /*
 BULK INSERT Agent
 FROM '/enterManaMockData/AgentBulk.csv'
